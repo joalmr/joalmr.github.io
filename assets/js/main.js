a8e615168a09f9d9819a14c8d640ab72a3238f25
Alonso Massa <jose.massa@inetum.com>
2023-01-03 14:32:35 -0500

    prueba

--- assets/js/main.js
+++ assets/js/main.js
@@ -253,6 +253,12 @@
    * Animation on scroll
    */
   window.addEventListener('load', () => {
+
+    alert('Prueba');
+    setTimeout(function(){
+      alert('Prueba 2');
+  }, 2000);
+
     AOS.init({
       duration: 1000,
       easing: 'ease-in-out',
