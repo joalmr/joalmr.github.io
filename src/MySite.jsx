a6326689a37071ae15d6008b9a5d9c2a249a4889
joalmr <joalmr@gmail.com>
2024-05-18 03:29:22 -0500

    change to react

---
+++ src/MySite.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "./pages/Footer"
+import { Presentation } from "./pages/Presentation"
+
+
+export const MySite = () => {
+    return (
+        <>
+            <Presentation />
+            <Footer />
+        </>
+    )
+}
---
+++ src/pages/presentation/Container.jsx
@@ -0,0 +1,19 @@
+
+
+export const Container = () => {
+    return (
+        <div className="container">
+            <img src="/icon.svg" alt="Skills" />
+            <h1>Alonso Massa</h1>
+            <p><span>Mobile Developer</span></p>
+            <div className="social-links">
+                <a href="https://www.linkedin.com/in/alonsomassa" target="_blank">
+                    <img src="assets/svg/linkedin.svg" />
+                </a>
+                <a href="https://github.com/joalmr" target="_blank">
+                    <img src="assets/svg/github.svg" />
+                </a>
+            </div>
+        </div>
+    )
+}
